2208c62d1039667ede236156cb25af09007ccb56
Natsunsn <Natsunan270@gmail.com>
2023-10-17 15:41:58 +0700

    first commit

---
+++ app4.js
@@ -0,0 +1,10 @@
+//Number
+let count = 10; // integer literal
+const blue = 0x0000ff;//hef ff
+const umask = 0o0022;//octal 22
+const roomType = 21.5;//decimal
+const c = 3.0e6;//3.0 x 10^6
+const e = 1.6e-16;//-1.6x10^-16
+// const nan = Nan;
+
+console.log(count, blue, umask, roomType, c, e);
---
+++ app5.js
@@ -0,0 +1,11 @@
+//Basic syntag od Strig
+let msg = "Hello";
+let fnam = "Mark";
+let lname = "Zuckerberg";
+let age ="10" +10;//1010
+let salaray = "10" - 7;//3
+let money = "Hello" - 10;//NaN
+console.log(msg + " " + fnam + " "+ age +" " +salaray +" " + money);
+console.log(lname.length);
+console.log(lname.toUpperCase());
+console.log(lname.toLocaleLowerCase());
